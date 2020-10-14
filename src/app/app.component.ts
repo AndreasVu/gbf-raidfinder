3c86d459ffc00959f85e1e45fddf2bbe53f1955e
AndreasVu <andreas.loyning.vu@gmail.com>
2020-10-14 15:25:06 +0200

    Added localstorage to store the selected raids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,35 @@ import { Raid } from '../models/raid.model'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  selectedRaids: Raid[] = [];
+  selectedRaids: Raid[] = this.getRaidsFromLocalStorage();
   title = 'raidfinder';
 
   constructor() {
 
   }
 
+  getRaidsFromLocalStorage(): Raid[] {
+    let store = localStorage.getItem('selectedRaids');
+    if (store) {
+      return JSON.parse(store);
+    } else return [];
+  }
+
   onRaidAdded(raid: AddRaidEntry) {
     if (!this.selectedRaids.includes(raid)) {
       this.selectedRaids.push(raid as Raid);
+
+      localStorage.setItem('selectedRaids', JSON.stringify(this.selectedRaids));
     }
   }
 
   onRaidRemoved(raid: Raid) {
     this.selectedRaids = this.selectedRaids.filter(obj => obj !== raid);
+    let oldArray = JSON.parse(localStorage.getItem('selectedRaids')) as Raid[];
+    localStorage.setItem('selectedRaids', JSON.stringify(
+      oldArray.filter(element => {
+        element != raid
+      })
+    ));
   }
 }
