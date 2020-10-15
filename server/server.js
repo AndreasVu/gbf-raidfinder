8141e427f0f74a51f30271458d43f2ac504860d8
AndreasVu <andreas.loyning.vu@gmail.com>
2020-10-15 16:26:47 +0200

    typo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const apiClient = new Twitter(
         "consumer_key": process.env.consumer_key,
         "consumer_secret": process.env.consumer_secret,
         "access_token": process.env.access_token,
-        "access_token_secret": process.env.access_token
+        "access_token_secret": process.env.access_token_secret
     }
 );
 const PORT = process.env.PORT || 5000
